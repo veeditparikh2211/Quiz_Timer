3e24e00db699ccc0243eb45fe9647da3b4c116ba
veeditparikh2211 <veeditparikh2211@gmail.com>
2021-12-17 20:28:22 -0500

    changes made for css and javascript file

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,90 +1,16 @@
-var btnelmt = document.querySelector("#start");
-var h1 = document.querySelector("#title");
-var info = document.createElement("div");
-var oderListEl = document.querySelector("#list");
-var divCont = document.querySelector(".divContainer");
-var timeElement = document.querySelector("#counter");
-var anserdiv = document.querySelector("#answer");
-var final = document.querySelector("#result");
-var finish = document.querySelector(".finish_page");
-var finalpage = document.querySelector(".Highscore");
-var errMsg = document.querySelector("#error");
-var initial = document.querySelector("#input");
-var initialAndScore = document.querySelector("#text");
-var wrapperElement = document.querySelector(".nav justify-content-center");
-var submit = document.querySelector(".btn btn-primary mb-2");
-
-var i = 0;
-
-
-function showquestion() {
-
-    var list = questions[i].title;
-    h1.textContent = list;
-    var Choice1 = questions[i].choices[0];
-
-
-    var li1 = document.createElement("li");
-    var li2 = document.createElement("li");
-    var li3 = document.createElement("li");
-    var li4 = document.createElement("li");
-
-
-    var Choice2 = questions[i].choices[1];
-    var Choice3 = questions[i].choices[2];
-    var Choice4 = questions[i].choices[3];
-
-
-    oderListEl.innerHTML = "";
-    li1.setAttribute("class", "all");
-
-    var btn = document.createElement('button');
-    btn.setAttribute("class", "all_btn")
-    btn.textContent = Choice1;
-    li1.appendChild(btn)
-    oderListEl.appendChild(li1);
-    divCont.appendChild(oderListEl);
-
-    var btn = document.createElement('button');
-    btn.setAttribute("class", "all_btn")
-    btn.textContent = Choice2;
-    li2.appendChild(btn)
-    oderListEl.appendChild(li2);
-    divCont.appendChild(oderListEl);
-
-    var btn = document.createElement('button');
-    btn.setAttribute("class", "all_btn")
-    btn.textContent = Choice3;
-    li3.appendChild(btn)
-    oderListEl.appendChild(li3);
-    divCont.appendChild(oderListEl);
-
-    var btn = document.createElement('button');
-    btn.setAttribute("class", "all_btn")
-    btn.textContent = Choice4;
-    li4.appendChild(btn)
-    oderListEl.appendChild(li4);
-    divCont.appendChild(oderListEl);
-
-
-
-    var allBtn = document.querySelectorAll(".all_btn")
-    allBtn.forEach(function(event) {
-        event.addEventListener("click", onclickHandler)
-    });
-
-}
 var counter = 75;
-var timeCount;
+var count;
 
+
+// Timer function to start the quiz timer
 function startTimer() {
 
-    timeCount = setInterval(function() {
+    count = setInterval(function() {
         counter--;
         var timeReset = timeElement.textContent = "Time:" + " " + counter;
         counter = counter;
         if (counter <= 0) {
-            clearInterval(timeCount);
+            clearInterval(count);
 
             timeElement.textContent = timeReset;
 
@@ -94,9 +20,104 @@ function startTimer() {
 
 document.addEventListener("click", function(event) {
     if (event.target === btnelmt) {
-        //wrapperElement.style.display = "none";
+        wrapperElement.style.display = "none";
         startTimer()
         showquestion();
     }
 
 })
+
+function onclickHandler(event) {
+
+    if (counter <= 0) {
+        clearInterval(count);
+        divCont.style.display = "none";
+        showresult();
+    }
+
+    var answertext = event.target.textContent;
+    console.log('answer is', answertext);
+
+    if (answertext === questions[i].answer) {
+        counter = counter;
+        anserdiv.setAttribute("style", "color : green");
+        anserdiv.textContent = "Correct";
+        console.log('question no.', questions[i].answer);
+
+    } else {
+        anserdiv.setAttribute("style", "color : red");
+        anserdiv.textContent = "Wrong";
+        counter = counter - 15;
+    }
+
+    console.log('value', i);
+    console.log('question', questions);
+
+    if (i < questions.length - 1) {
+        i++;
+        setTimeout(function() {
+            showquestion();
+            anserdiv.textContent = "";
+        }, 1000)
+    } else {
+        setTimeout(function() {
+
+            anserdiv.textContent = "";
+
+            showresult();
+            clearInterval(count);
+        }, 500)
+        divCont.innerHTML = "";
+    }
+
+    // function to store the result with in the localstorage
+    function showresult() {
+        finish.style.visibility = "visible";
+        timeElement.textContent = "Time" + "" + counter;
+        var score = counter;
+        localStorage.getItem(score)
+        console.log("score is ", score);
+        final.textContent = "Your finally score is: " + score;
+
+        localStorage.setItem("score", score)
+    }
+}
+
+// function to print the final score 
+function finalscore() {
+    var scores = localStorage.getItem("score");
+    var initial = localStorage.getItem("Initial");
+
+    if (scores && initial === "") {
+        return
+    }
+    finish.textContent = "";
+    var finalpage = document.querySelector(".Highscore");
+    finalpage.style.visibility = "visible";
+    var initialAndScore = document.querySelector("#letter");
+
+    initialAndScore.value = initial + ":" + " " + scores;
+    initialAndScore.setAttribute("style", "text-align : center");
+    console.log("final score is " + scores);
+
+}
+document.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var initialInput = document.querySelector("#input").value;
+    if (initialInput === "") {
+        errMsg.setAttribute("style", "color: red")
+        errMsg.textContent = "Initial input field cannot be empty"
+    } else {
+        errMsg.textContent = "";
+        localStorage.getItem(initialInput)
+        localStorage.setItem("Initial", initialInput)
+        finalscore()
+    }
+
+})
+
+// function to restart the quiz
+function start() {
+    location.reload();
+
+}
--- assets/questions.js
+++ assets/questions.js
@@ -1,3 +1,21 @@
+var btnelmt = document.querySelector("#start");
+var h1 = document.querySelector("#title");
+var info = document.createElement("div");
+var oderList = document.querySelector("#list");
+var divCont = document.querySelector(".container");
+var timeElement = document.querySelector("#time");
+var anserdiv = document.querySelector("#answer");
+var final = document.querySelector("#result");
+var finish = document.querySelector(".finish_page");
+var finalpage = document.querySelector(".Highscore");
+var errMsg = document.querySelector("#error");
+var initial = document.querySelector("#input").value;
+var initialAndScore = document.querySelector("#letter");
+var wrapperElement = document.querySelector(".wrapper");
+var submit = document.querySelector(".btn btn-primary mb-2");
+
+
+// list of questions
 var questions = [{
         title: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
@@ -9,15 +27,15 @@ var questions = [{
         answer: "parentheses",
     },
     {
-        title: "There are 3 different ways in which a JavaScript code can be involved in an HTML file. Selct the one that's not correct.",
-        choices: ["Inline", "Import", "External", "Internal"],
-        answer: "Import",
+        title: "Array in Javascript can be use to store",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of Above"],
+        answer: "All of Above",
     },
     {
-        title: "How to create an array in js ?",
-        choices: ["var A[]=", "var A{}=", "var A=[]", "var A={}"],
+        title: "String values must be enclosed within________when being assigned to variables. ?",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
 
-        answer: "var A=[]",
+        answer: "quotes",
     },
     {
         title: "HTML element that can be accessed in a Javascript code: Chose the one that will return an array of elements",
@@ -27,3 +45,59 @@ var questions = [{
         answer: "querySelectorAll()",
     }
 ]
+
+var i = 0;
+
+// function created to list the question with answers 
+function showquestion() {
+
+    var list = questions[i].title;
+    h1.textContent = list;
+
+    var li1 = document.createElement("li");
+    var li2 = document.createElement("li");
+    var li3 = document.createElement("li");
+    var li4 = document.createElement("li");
+
+    var Choice1 = questions[i].choices[0];
+    var Choice2 = questions[i].choices[1];
+    var Choice3 = questions[i].choices[2];
+    var Choice4 = questions[i].choices[3];
+
+    oderList.innerHTML = '';
+    li1.setAttribute("class", "all");
+
+    var btn = document.createElement('button');
+    btn.setAttribute("class", "all_btn")
+    btn.textContent = Choice1;
+    li1.appendChild(btn)
+    oderList.appendChild(li1);
+    divCont.appendChild(oderList);
+
+    var btn = document.createElement('button');
+    btn.setAttribute("class", "all_btn")
+    btn.textContent = Choice2;
+    li2.appendChild(btn)
+    oderList.appendChild(li2);
+    divCont.appendChild(oderList);
+
+    var btn = document.createElement('button');
+    btn.setAttribute("class", "all_btn")
+    btn.textContent = Choice3;
+    li3.appendChild(btn)
+    oderList.appendChild(li3);
+    divCont.appendChild(oderList);
+
+    var btn = document.createElement('button');
+    btn.setAttribute("class", "all_btn")
+    btn.textContent = Choice4;
+    li4.appendChild(btn)
+    oderList.appendChild(li4);
+    divCont.appendChild(oderList);
+
+    var allBtn = document.querySelectorAll(".all_btn")
+    allBtn.forEach(function(event) {
+        event.addEventListener("click", onclickHandler)
+    });
+
+}
